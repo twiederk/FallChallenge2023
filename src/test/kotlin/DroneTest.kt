76806ce99dd7c3317c391a11a71b030429db0fcd
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-18 23:22:12 +0100

    Implemented nearestVisibleCreature with filtering already scanned creatures

--- src/test/kotlin/DroneTest.kt
+++ src/test/kotlin/DroneTest.kt
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test
 class DroneTest {
 
     @Test
-    fun should_find_nearestCreatureToScan() {
+    fun should_find_nearestCreatureToScan_with_none_scanned_creatures() {
         // arrange
-        val drone = Drone(droneId = 0, Point2D(3333, 500), 0, 30)
         val visibleCreatures = listOf(
             VisibleCreature(
                 id = 0,
@@ -33,12 +32,51 @@ class DroneTest {
                 creatureVelocity = Point2D(0, -200)
             ),
         )
+        val drone = Drone(droneId = 0, Point2D(3333, 500), 0, 30)
 
         // act
-        val nearestVisibleCreature = drone.nearestCreatureToScan(visibleCreatures, listOf<Int>())
+        val nearestVisibleCreature = drone.nearestCreatureToScan(visibleCreatures, listOf())
 
         // assert
         assertThat(nearestVisibleCreature).isEqualTo(visibleCreatures[1])
+    }
+
+    @Test
+    fun should_find_nearestCreatureToScan_with_scanned_creatures() {
+        // arrange
+        val visibleCreatures = listOf(
+            VisibleCreature(
+                id = 0,
+                creatureId = 3,
+                creaturePosition = Point2D(1195, 3926),
+                creatureVelocity = Point2D(0, -200)
+            ),
+            VisibleCreature(
+                id = 1,
+                creatureId = 9,
+                creaturePosition = Point2D(2691, 3920),
+                creatureVelocity = Point2D(0, -200)
+            ),
+            VisibleCreature(
+                id = 2,
+                creatureId = 8,
+                creaturePosition = Point2D(7308, 3920),
+                creatureVelocity = Point2D(0, -200)
+            ),
+            VisibleCreature(
+                id = 3,
+                creatureId = 3,
+                creaturePosition = Point2D(8804, 3926),
+                creatureVelocity = Point2D(0, -200)
+            ),
+        )
+        val drone = Drone(droneId = 0, Point2D(3333, 500), 0, 30)
+
+        // act
+        val nearestVisibleCreature = drone.nearestCreatureToScan(visibleCreatures, listOf(9))
+
+        // assert
+        assertThat(nearestVisibleCreature).isEqualTo(visibleCreatures[0])
     }
 
 }
